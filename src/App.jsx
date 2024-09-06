391d7f67251d32e39d9cb1822f40b60f74eeb1e2
Arce-hub <maricela.mendoza24a@udgvirtual.udg.mx>
2024-09-05 20:15:31 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Banner from './Components/Banner/Banner';
 import Buildings from './Components/Buildings/Buildings';
 import VirtualMeetingAd from './Components/VirtualMeetingAd/VirtualMeetingAd';
+import Footer from './Components/Footer/footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Banner />
       <Buildings />
       <VirtualMeetingAd />
+      <Footer />
     </div>
   );
 }
